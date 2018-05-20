ee71ed2ff4a907a82e06b465396254192d3ac3b3
Edwinguo <edwinguo@Edwinguos-MacBook-Pro.local>
2018-05-19 20:57:48 -0400

    Update the program, fix some racing bug

--- tendermint/FunAlien.go
+++ tendermint/FunAlien.go
@@ -22,7 +22,6 @@ import (
 // TODO, change this back to 10000 once finished development
 var numOfCommands = 3
 
-
 var commanderCounter = 1
 var aliens map[Alien]Status
 var cityLookup map[string]map[string]bool
@@ -41,11 +40,12 @@ func AlienCommander(iters int) <-chan int {
                         command, more := <-toCommanderSignalChan
                         fmt.Printf("AlienCommander: I will trigger the %dth round of attack\n", commanderCounter)
                         if more {
-                                if (command == "continue" && commanderCounter < iters && len(aliens) > 0) {
+                                if (command == "continue" && commanderCounter <= iters && len(aliens) > 0) {
                                         c <- counter
                                         commanderCounter++
                                         //time.Sleep(1 * time.Second)
                                 } else {
+                                        fmt.Println("AlienCommander:  commander is exiting.....command", command, "counter: ", commanderCounter, "lens, ", len(aliens))
                                         close(c)
                                 }
                         } else {
@@ -55,78 +55,6 @@ func AlienCommander(iters int) <-chan int {
                 }
         }()
         return c
-}
-
-
-func alienMove(alien Alien, lookupCity map[string]map[string]bool, cityAlienLookup map[string]map[Alien]bool) {
-        neighbour := lookupCity[aliens[alien].city]
-        city := util.RandMove(neighbour)
-        fmt.Println("Aggregator: what is the neighbour: ", neighbour, " for alien ", aliens[alien])
-        fmt.Println("Aggregator: what is the new city: ", city, " for alien ", aliens[alien])
-        if city != "" {
-                // move to the new city
-                status := aliens[alien]
-                status.city = city
-                aliens[alien] = status
-                // update cityAlienlookup
-                aliensLookup := cityAlienLookup[city]
-                if len(aliensLookup) == 0{
-                        newMap := make(map[Alien]bool)
-                        newMap[alien] = true
-                        cityAlienLookup[city] = newMap
-                } else{
-                        aliensLookup[alien] = true
-                }
-
-        }
-}
-
-// after everymove, check whether the city has two aliens stay there,
-// if yes, then we need to clean up the lookupcity by update each city's
-// neighbour and also update cityAlienlookup to remove the destroyed aliens
-// and cities
-func cityCheckup(alien Alien, lookupCity map[string]map[string]bool, cityAlienLookup map[string]map[Alien]bool) {
-        city := aliens[alien].city
-        alis := cityAlienLookup[city]
-        fmt.Println("Aggregator: I'm in cityCheckup, aliens: ", aliens)
-        fmt.Println("Aggregator: I'm in cityCheckup, lookupCity", lookupCity)
-        fmt.Println("Aggregator: I'm in cityCheckup, cityAlienLookup",  cityAlienLookup)
-
-        if (len(alis) > 1){
-                // Clean up and destroy
-                // send a termination signal to the aliens
-                // update lookupcity
-                // and citya connect to cityb,
-                // then cityb must connect to citya
-                for alien, _ := range alis {
-                        fmt.Println("Aggregator: I'm in cityCheckup, and I'm close channels",  alis)
-                        close(alien.commandChan)
-                        delete(alis, alien)
-                        delete(aliens, alien)
-                }
-
-                delete(cityAlienLookup, city)
-
-                for ci, _ := range lookupCity[city]{
-                        delete(lookupCity[city], ci)
-                }
-
-                delete(lookupCity, city)
-        }
-}
-
-func consumer(alien Alien, terminatorChan chan int, aggregatorSignalChan chan Alien) {
-        for {
-                _, more := <-alien.commandChan
-                if more {
-                        fmt.Println("Consumer: Alien ", alien.name, "had been activated and is in motion!")
-                        aggregatorSignalChan <- alien
-                } else {
-                        fmt.Println("Consumer: I'm done: ", alien.name)
-                        terminatorChan <- 1
-                        return
-                }
-        }
 }
 
 func currentRoundFinish() bool {
@@ -146,12 +74,104 @@ func currentRoundFinish() bool {
         return true
 }
 
+func alienMove(alien Alien, lookupCity map[string]map[string]bool, cityAlienLookup map[string]map[Alien]bool) {
+        oldCity := aliens[alien].city
+        neighbour := lookupCity[oldCity]
+        fmt.Println("Aggregator: what is the neighbour: ", neighbour, " for alien ", alien, ",", aliens[alien])
+
+        city := util.RandMove(neighbour)
+        fmt.Println("Aggregator: what is the new city: ", city, " for alien ", aliens[alien])
+        if city != "" {
+                // remove previous alien from the old city from cityAlienlookup
+                delete(cityAlienLookup[oldCity], alien)
+
+                // move to the new city
+                status := aliens[alien]
+                status.city = city
+                aliens[alien] = status
+
+                // update cityAlienlookup
+                aliensLookup := cityAlienLookup[city]
+                if len(aliensLookup) == 0{
+                        newMap := make(map[Alien]bool)
+                        newMap[alien] = true
+
+                        cityAlienLookup[city] = newMap
+                } else{
+                        aliensLookup[alien] = true
+                }
+
+        }
+}
+
+// after everymove, check whether the city has two aliens stay there,
+// if yes, then we need to clean up the lookupcity by update each city's
+// neighbour and also update cityAlienlookup to remove the destroyed aliens
+// and cities
+func cityCheckup(alien Alien, lookupCity map[string]map[string]bool, cityAlienLookup map[string]map[Alien]bool) {
+        city := aliens[alien].city
+        alis := cityAlienLookup[city]
+        fmt.Println("Aggregator: I'm in cityCheckup, aliens: ", aliens)
+        fmt.Println("Aggregator: I'm in cityCheckup, cityAlienLookup",  cityAlienLookup)
+
+        if (len(alis) > 1){
+                ali1 := ""
+                ali2 := ""
+
+                // Clean up and destroy
+                // send a termination signal to the aliens
+                // update lookupcity
+                // and citya connect to cityb,
+                // then cityb must connect to citya
+                indexer := 0
+                for alien, _ := range alis {
+                        fmt.Println("Aggregator: deleting aliens.......", alien)
+                        close(alien.commandChan)
+                        delete(alis, alien)
+                        delete(aliens, alien)
+                        if indexer == 0{
+                                ali1 = alien.name
+                                indexer++
+                        } else {
+                                ali2 = alien.name
+                        }
+                }
+
+                fmt.Printf("Aggregator: %s has been destroyed by %s and %s!\n", city, ali1, ali2)
+
+                delete(cityAlienLookup, city)
+
+                for _, cmap := range lookupCity{
+                        delete(cmap, city)
+                }
+
+                delete(lookupCity, city)
+        }
+        fmt.Println("Aggregator: I'm in cityCheckup, lookupCity", lookupCity)
+}
+
+func consumer(alien Alien, terminatorChan chan int, aggregatorSignalChan chan Alien) {
+        for {
+                _, more := <-alien.commandChan
+                if more {
+                        fmt.Println("Consumer: Alien ", alien.name, "had been activated and is in motion!")
+                        aggregatorSignalChan <- alien
+                } else {
+                        fmt.Println("Consumer: I'm done: ", alien.name)
+                        terminatorChan <- 1
+                        return
+                }
+        }
+}
+
+
+
 // aggregator is in charge of collect the info from alien consumer and send the signal to aliencommander
 func Aggregator(ch chan Alien) {
         go func() {
                 fmt.Println("Aggregator, I'm in work...")
                 for {
-                        alien, more := <- ch
+                        alien, more := <- aggregatorSignalChan
                         if more{
                                 fmt.Println("Aggregator: receive work...")
 
@@ -174,7 +194,7 @@ func Aggregator(ch chan Alien) {
                                 // if all aliens are destroyed, then end of the game
                                 if (len(aliens) == 0) {
                                         fmt.Println("ALl the aliens had been destroyed, terminating the game")
-                                        close(ch)
+                                        close(aggregatorSignalChan)
                                 } else if (currentRoundFinish()){
                                         fmt.Println("ALl aliens had made their move for the current round... Next round will start shortly...")
                                         toCommanderSignalChan <- "continue"
@@ -185,7 +205,7 @@ func Aggregator(ch chan Alien) {
                         } else {
                                 toCommanderSignalChan <- "done"
                                 fmt.Println("ALl the aliens had been destroyed, terminating the game, done.....")
-                                close(ch)
+                                close(aggregatorSignalChan)
                         }
                 }
         }()
@@ -221,6 +241,7 @@ func PassCommandToAlien(ch <-chan int) {
         go func() {
                 fmt.Println("PassCommandToAlien: I'm in work mode now")
                 for i := range ch {
+                        fmt.Println("PassCommandToAlien: what is the aliens now:", aliens)
                         for alien, _ := range aliens {
                                 alien.commandChan <- i
                         }
@@ -326,7 +347,7 @@ func main() {
 
         PassCommandToAlien(commandChannel)
 
-        Aggregator(aggregatorSignalChan)
+        Aggregator()
 
         Terminator(numOfAliens)
 
